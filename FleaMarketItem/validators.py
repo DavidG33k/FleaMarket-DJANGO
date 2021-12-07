6449cde9412991f20b75b8cae262eaa7f1a76c31
palla98 <pallaria98@gmail.com>
2021-12-07 16:53:49 +0100

    re-model and validator

--- FleaMarketItem/validators.py
+++ FleaMarketItem/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def validate_price(value: int) -> None:
     if not value >= 0:
-        raise ValidationError('Price can\'t be negative')
-    if not value <= 100000000000 - 1:
-        raise ValidationError('Price can\'t be greater than 1,000,000.00')
+        raise ValidationError('It is not possible to use a negative price')
+    if not value <= 100000:
+        raise ValidationError('The price must be less 100.000')
 
--- tests/FleaMarketItem/test_models.py
+++ tests/FleaMarketItem/test_models.py
@@ -2,23 +2,17 @@ import pytest
 from mixer.backend.django import mixer
 from django.core.exceptions import ValidationError
 
-#name, description, condition, brand
-def test_item_name_of_length_25_raises_exception(db):
-    item = mixer.blend('FleaMarketItem.Item',name='A'*26)
+
+# name
+def test_item_name_of_length_31_raises_exception(db):
+    item = mixer.blend('FleaMarketItem.Item', name='A'*31)
     with pytest.raises(ValidationError) as err:
         item.full_clean()
-    assert 'at most 25 characters' in '\n'.join(err.value.messages)
 
-def test_item_name_of_less_than_1_raises_exception(db):
-    item = mixer.blend('FleaMarketItem.Item',name='')
-    with pytest.raises(ValidationError) as err:
-        item.full_clean()
-    assert 'no empty paramter name' in '\n'.join(err.value.messages)
 
-def test_item_name_forbidden_characters_raises_exception(db):
-    item = mixer.blend('FleaMarketItem.Item',name='<')
-    with pytest.raises(ValidationError) as err:
-        item.full_clean()
-    assert 'not special character in prm name' in '\n'.join(err.value.messages)
-
+def test_item__name_only_blank_characters_raises_exception(db):
+    shopping_list_item = mixer.blend('item_shopping_list.ShoppingListItem', name='.', category='Smartphone',
+                                     description='ciao', quantity=1)
+    with pytest.raises(ValidationError):
+        shopping_list_item.full_clean()
 
