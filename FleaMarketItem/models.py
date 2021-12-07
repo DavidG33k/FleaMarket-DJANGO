aaa0723007105105d5bc901716c5e6853a0050c9
palla98 <pallaria98@gmail.com>
2021-12-07 17:27:28 +0100

    temporary final model and initial tests for model

--- FleaMarketItem/models.py
+++ FleaMarketItem/models.py
@@ -7,19 +7,19 @@ from django.utils.translation import gettext_lazy as _
 
 # condition enum definition
 class ConditionStatus(models.TextChoices):
-    AS_NEW = 'AN', _('AS_NEW')
-    GOOD_CONDITION = 'GC', _('GOOD_CONDITION')
-    ACCEPTABLE_CONDITION = 'AC', _('ACCEPTABLE_CONDITION')
+    AS_NEW = 'AS_NEW',
+    GOOD_CONDITION = 'GOOD_CONDITION'
+    ACCEPTABLE_CONDITION = 'ACCEPTABLE_CONDITION'
 
 
 class Item(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, validators=[RegexValidator(r'^[A-Za-z0-9]{1,30}$')])
-    description = models.TextField(blank=True, max_length=200, validators=[RegexValidator(r'^[A-Za-z0-9\(\)\!\,\è\:\;\'\"\. ]{1,200}$')])
+    name = models.CharField(max_length=30, validators=[RegexValidator(r'^[A-Za-z0-9]+$')])
+    description = models.TextField(blank=True, max_length=200, validators=[RegexValidator(r'^[A-Za-z0-9\(\)\!\,\è\:\;\'\"\. ]+$')])
     condition = models.CharField(ConditionStatus.choices, max_length=30)
-    brand = models.CharField(max_length=20, validators=[RegexValidator(r'^[A-Za-z0-9\_\-\(\)]{1,20}$')])
+    brand = models.CharField(max_length=20, validators=[RegexValidator(r'^[A-Za-z\_\-\(\)]+$')])
     price = models.IntegerField(validators=[validate_price])
-    category = models.CharField(max_length=30, validators=[RegexValidator(r'^[A-Za-z0-9\_\-\(\) ]{1,30}$')])
+    category = models.CharField(max_length=30, validators=[RegexValidator(r'^[A-Za-z\_\-\(\) ]+$')])
 
     def __str__(self) -> str:
         return str(self.user) + " - " + self.name
--- tests/FleaMarketItem/test_models.py
+++ tests/FleaMarketItem/test_models.py
@@ -4,15 +4,27 @@ from django.core.exceptions import ValidationError
 
 
 # name
-def test_item_name_of_length_31_raises_exception(db):
+def test_item_name_of_length_more_than_30(db):
     item = mixer.blend('FleaMarketItem.Item', name='A'*31)
-    with pytest.raises(ValidationError) as err:
+    with pytest.raises(ValidationError):
         item.full_clean()
 
 
-def test_item__name_only_blank_characters_raises_exception(db):
-    shopping_list_item = mixer.blend('item_shopping_list.ShoppingListItem', name='.', category='Smartphone',
-                                     description='ciao', quantity=1)
+def test_item_name_length_equal_to_zero(db):
+    item = mixer.blend('FleaMarketItem.Item', name='')
     with pytest.raises(ValidationError):
-        shopping_list_item.full_clean()
+        item.full_clean()
 
+
+# description
+def test_item_description_of_length_more_than_200(db):
+    item = mixer.blend('FleaMarketItem.Item', description='A'*200)
+    with pytest.raises(ValidationError):
+        item.full_clean()
+
+
+# condition
+def test_item_condition_different_enum_value(db):
+    item = mixer.blend('FleaMarketItem.Item', condition='SBAGLIATO')
+    with pytest.raises(ValidationError):
+        item.full_clean()
